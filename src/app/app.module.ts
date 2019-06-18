870fbf66dcc0fc887f645273a449befd083cc273
ytzlax <ytzlax@gmail.com>
2019-06-18 11:25:51 +0300

    start infrastructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularCesiumModule } from 'angular-cesium';
+import { PolygonLayerComponent } from './components/polygon-layer/polygon-layer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,PolygonLayerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularCesiumModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
